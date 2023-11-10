39b42915554aa7dee7dfb5a305458578a8490abe
Aleksandar Atanasov <aatanasov1@godaddy.com>
2023-11-10 13:30:42 +0200

    Update tests

--- generate_dag/tests/test_generate_dag.py
+++ generate_dag/tests/test_generate_dag.py
@@ -7,6 +7,7 @@ import yaml
 from generate_dag.generate_dag import _create_dag_code
 
 VALIDATE_CONFIG_PATH = 'generate_dag/validate_config.yaml'
+REPORT_PATH = '/tmp/validation_report.yaml'
 
 
 def read_validate_config():
@@ -14,20 +15,38 @@ def read_validate_config():
         return yaml.safe_load(f)
 
 
-def get_dags():
-    dag_files = {}
-    for full_path in glob('**/dags/*.py', recursive=True):
-        dag_name = full_path.split('/')[0]
-        if dag_name not in read_validate_config().get('exclude', []):
-            dag_files[dag_name] = full_path
-    return dag_files
-
-
-class TestExportMetlFramework(TestCase):
+class TestValidateDags(TestCase):
 
     def setUp(self) -> None:
-        self.dags = get_dags()
+        self.report = {
+            'summary': {
+                'ok': 0,
+                'error': 0,
+                'skipped': 0
+            },
+            'passed': [],
+            'failed': [],
+            'skipped': [],
+        }
+        self.dags = self.get_dags()
         self.maxDiff = None
+
+    def tearDown(self) -> None:
+        self.report['summary']['ok'] = len(self.report['passed'])
+        self.report['summary']['error'] = len(self.report['failed'])
+        self.report['summary']['skipped'] = len(self.report['skipped'])
+        with open(REPORT_PATH, 'w') as yaml_file:
+            yaml.dump(self.report, yaml_file, default_flow_style=False, sort_keys=False)
+
+    def get_dags(self):
+        dag_files = {}
+        for full_path in glob('**/dags/*.py', recursive=True):
+            dag_name = full_path.split('/')[0]
+            if dag_name not in read_validate_config().get('exclude', []):
+                dag_files[dag_name] = full_path
+            else:
+                self.report['skipped'].append(dag_name)
+        return dag_files
 
     def test_validate_dag_code(self):
         mock_open_calls = MagicMock()
@@ -37,4 +56,10 @@ class TestExportMetlFramework(TestCase):
                     actual = f.read()
                 with patch('generate_dag.generate_dag._write_file', mock_open_calls) as m:
                     expected = _create_dag_code(dag)
-                    self.assertMultiLineEqual(actual, expected)
+                    try:
+                        self.assertMultiLineEqual(actual, expected)
+                        self.report['passed'].append(dag)
+                    except AssertionError as e:
+                        self.report['failed'].append(dag)
+                        raise AssertionError(e)
+
