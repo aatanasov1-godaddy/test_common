fd51e032af4ec475fb058fd9a5a7cfb191ae24d2
Aleksandar Atanasov <aatanasov1@godaddy.com>
2023-11-10 13:31:37 +0200

    Update dag

--- customer_intent/dags/customer_intent.py
+++ customer_intent/dags/customer_intent.py
@@ -50,7 +50,6 @@ SKIP_EXTRACT = """{{ dag_run.conf['skip_extract'] if dag_run != None and dag_run
 DAG_CONFIG = Variable.get(f'{DAG_ID}_config', default_var=json.dumps({'output_suffix': '_metl'}))
 DEPENDENCIES = [
     'websites_and_marketing_cln.website_cln',
-    'enterprise.dim_entitlement',
     'enterprise.dim_subscription',
 ]
 
